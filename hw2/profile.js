ee05826aca77328049ed855bdf233b943eea9991
yusong-shen <yusongtju@gmail.com>
2016-09-08 14:08:07 -0500

    clear the input field after submitting

--- hw2/profile.js
+++ hw2/profile.js
@@ -9,7 +9,6 @@ window.onload = function() {
 		'displayName' : '',
 		'phone' : '* Phone number format should look like : 123-123-1234\n',
 		'email' : '* Email format should look like : name@example.com\n',
-		'bday' : '* Only individuals 18 years of age or older on the day of registration are allowed to register\n',
 		'password' : '* Passwords you enter are not equal\n',
 		'zipcode' : '* Zipcode should be 5 digits like 77005\n'
 	};
@@ -23,58 +22,16 @@ window.onload = function() {
 	function validateRe(re, value) {
 		return (value.match(re) !== null);
 	}
-	function validateBday(date) {
-		// Only individuals 18 years of age or older on the day of 
-		// registration are allowed to register
-		var eighteenYearsAgo = moment().subtract(18, 'years');
-		var birthday = moment(date);
-
-		if (!birthday.isValid()) {
-		    return false;    
-		}
-		else if (eighteenYearsAgo.isAfter(birthday)) {
-		    return true;    
-		}
-		else {
-		    return false;
-		}
-	}
 	function validatePassword(pw, pw_cf) {
 		return pw === pw_cf;		
 	}
-	function isValidated(form) {
-		// incase the argument is missing
-		if (!form) {
-			form = document.getElementById('userForm');
-		}
-		var alertMsg = 'something is wrong!';
-		// validate by regular expression
-		// 123-123-1234
-		var phoneRe = /^\d{3}[-]\d{3}[-]\d{4}$/;
-		var emailRe = /[^\s@]+@[^\s@]+\.[^\s@]+/;
-		var zipcodeRe = /^\d{5}/;
-		// perform validations
-		var isPhoneValidated = validateRe(phoneRe, form.phone.value);
-		var isEmailValidated = validateRe(emailRe, form.email.value);
-		var isZipValidated = validateRe(zipcodeRe, form.zipcode.value);
-		var isBdayValidated = validateBday(form.bday.value);
-		var isPwValidated = validatePassword(form.password.value, form.password_confirmation.value);
-		if (isEmailValidated && isPhoneValidated && isBdayValidated &&
-			isZipValidated && isPwValidated) {
-			return true;
-		} else {
-			window.alert(alertMsg);
-			return false;
-		}
-	}
-	// record the timestamp in millisecond when page is loaded
-	function setDate() {
-		document.getElementById('timestamp').value = Date.now();
-	}
-	setDate();
 	function clearContent() {
 		var allInputs = document.getElementsByTagName('input');
-		allInputs.value = '';
+		Array.prototype.forEach.call(allInputs, function(item) {
+			if (item.type !== 'submit') {
+				item.value = '';
+			}
+		});
 	}
 	// return the string indicating sucessfully updated
 	function updatedMsg(field, oldVal, newVal) {
@@ -105,5 +62,8 @@ window.onload = function() {
 				updateField(item, oldElement, newElement);
 			}
 		});
+
+		// clear all the contents
+		clearContent();
 	}
 }
