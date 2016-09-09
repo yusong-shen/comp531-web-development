1834d424d72274fd91bfb0d614fab94f2f1d6a79
yusong-shen <yusongtju@gmail.com>
2016-09-08 22:59:19 -0500

    move car and sun across canvas

--- inclass-6/README.md
+++ inclass-6/README.md
@@ -1,5 +1,5 @@
-In-Class Exercise 06: Skyline Game
-Improve the skyline game
+#In-Class Exercise 06: Skyline Game
+##Improve the skyline game
 1. Not all lights are on in each building
 2. Mouse click on a building makes that building grow taller
 3. Add the Sun and have it move across the sky
--- inclass-6/skyline.js
+++ inclass-6/skyline.js
@@ -1,47 +1,83 @@
 'use strict'
 
-var createApp = function(canvas) { 
-	var c = canvas.getContext("2d");
+var modulo = function (x, y) {
+    return ((x % y) + y) % y;
+};
 
-	// Create the ground
-	var floor = canvas.height/2
-	var grad = c.createLinearGradient(0,floor,0,canvas.height)
-	grad.addColorStop(0, "green")
-	grad.addColorStop(1, "black")
-	c.fillStyle=grad
-	c.fillRect(0, floor, canvas.width, canvas.height)
+var createApp = function (canvas) {
+    var c = canvas.getContext("2d");
+    var floor = canvas.height / 2;
+    var carWidth = 70;
+    var carHeight = 30;
+    var carX = canvas.width - carWidth;
+    var carY = floor - carHeight;
+    var dx = 0.1 * canvas.width;
+    var sunX = 100;
+    var sunY = 50;
 
-	// common size for windows
-	var windowSpacing = 2, floorSpacing = 3
-	var windowHeight = 5, windowWidth = 3
+    // Create the ground
+    var grad = c.createLinearGradient(0, floor, 0, canvas.height);
+    grad.addColorStop(0, "green");
+    grad.addColorStop(1, "black");
+    c.fillStyle = grad;
+    c.fillRect(0, floor, canvas.width, canvas.height);
 
-	// colors of buildings
-	var blgColors = [ 'red', 'blue', 'gray', 'orange'] 
+    // common size for windows
+    var windowSpacing = 2, floorSpacing = 3;
+    var windowHeight = 5, windowWidth = 3;
 
-	//build a building
-	var build = function() { 
-		var x0 = Math.random()*canvas.width
-		var blgWidth = (windowWidth+windowSpacing) * Math.floor(Math.random()*10)
-		var blgHeight = Math.random()*canvas.height/2
+    // colors of buildings
+    var blgColors = ['red', 'blue', 'gray', 'orange'];
 
-		c.fillStyle= blgColors[ Math.floor(Math.random()*blgColors.length)]
-		c.fillRect(x0, floor - blgHeight, blgWidth, blgHeight)
-		c.fillStyle="yellow"
-		for (var y = floor - floorSpacing; y > floor - blgHeight; y -= floorSpacing + windowHeight) {
-			for (var x = windowSpacing; x < blgWidth - windowWidth; x += windowSpacing + windowWidth) {
-				c.fillRect(x0 + x, y - windowHeight, windowWidth, windowHeight)
-			}
-		}
-	}
+    //build a building
+    var build = function () {
+        var x0 = Math.random() * canvas.width;
+        var blgWidth = (windowWidth + windowSpacing) * Math.floor(Math.random() * 10);
+        var blgHeight = Math.random() * canvas.height / 2;
+        // clear the canvas above floor
+        c.clearRect(0, 0, canvas.width, canvas.height / 2);
 
-	return {
-		build: build
-	}
-}
+        // draw the sun
+        var radius = 30;
+        c.beginPath();
+        c.arc(sunX, sunY, radius, 0, 2 * Math.PI, false);
+        sunX = modulo(sunX + dx, canvas.width);
+        c.fillStyle = "orange";
+        c.fill();
 
-window.onload = function() {
-	var app = createApp(document.querySelector("canvas"))
-	document.getElementById("build").onclick = app.build
-}
+        // draw the building
+        c.fillStyle = blgColors[Math.floor(Math.random() * blgColors.length)];
+        c.fillRect(x0, floor - blgHeight, blgWidth, blgHeight);
+        c.fillStyle = "yellow";
+        var light = true;
+        for (var y = floor - floorSpacing; y > floor - blgHeight; y -= floorSpacing + windowHeight) {
+            for (var x = windowSpacing; x < blgWidth - windowWidth; x += windowSpacing + windowWidth) {
+                // not all lines in each building
+                if (light) {
+                    c.fillRect(x0 + x, y - windowHeight, windowWidth, windowHeight);
+                    light = false;
+                } else {
+                    light = true;
+                }
+            }
+        }
+
+        // draw the car
+        var carImg = new Image();
+        carImg.src = "cartoon-cars.png";
+        c.drawImage(carImg, carX, carY, carWidth, carHeight);
+        carX = modulo(carX - dx, canvas.width);
+    };
+
+    return {
+        build: build
+    }
+};
+
+window.onload = function () {
+    var app = createApp(document.querySelector("canvas"));
+    document.getElementById("build").onclick = app.build;
+    setInterval(app.build, 500);
+};
 
 
