0778e41e8874d91dcd3da7ef3e94432a55e68edd
yusong-shen <yusongtju@gmail.com>
2016-09-15 16:25:06 -0500

    write the tests using chai

--- inclass-8/src/particle.spec.js
+++ inclass-8/src/particle.spec.js
@@ -9,6 +9,10 @@ describe('Particle Functionality', () => {
         expect(p).to.be.ok
         expect(p.missingAttribute).to.not.be.ok
         // check position, velocity, acceleration, mass
+        expect(p.position).to.be.ok
+        expect(p.velocity).to.be.ok
+        expect(p.acceleration).to.be.ok
+        expect(p.mass).to.be.ok
     })
 
     it('should update the position by the velocity', () => {
@@ -25,7 +29,10 @@ describe('Particle Functionality', () => {
 
     it('should update the velocity by the acceleration', () => {
         // similar to the previous check
-        expect(1).to.equal(1)
+        // expect(1).to.equal(1)
+        const p = particle({ velocity: [1, 1], acceleration: [0.5, -0.5] })
+        const { velocity } = update(p, 1.0)
+        expect(velocity).to.equal([1.5, 0.5]) 
     })
 
     it('particles should wrap around the world', () => {
@@ -33,7 +40,18 @@ describe('Particle Functionality', () => {
         // of the canvas area.  update() should
         // bring the particle back inside
         // check all four sides
-        expect(1).to.equal(1)
+        // expect(1).to.equal(1)
+        // update(p, 3.521, { width:111, height: 222 })
+        var width = 800
+        var height = 800
+        const p = particle({ position: [900, 865], velocity: [0.5, -0.5] })
+        const { position } = update(p, 1)
+        console.log(position)
+        expect(position[0] <= width).to.be.true
+        expect(position[0] >= 0).to.be.true
+        expect(position[1] <= height).to.be.true
+        expect(position[1] >= 0).to.be.true
+
     })
 
 })
