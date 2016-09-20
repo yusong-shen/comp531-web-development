714243ceb7a53e3c0eea184192ed09c6ca7301c9
yusong-shen <yusongtju@gmail.com>
2016-09-20 15:29:16 -0500

    Cancel button will clear the content of post area

--- hw3/dist/main.js
+++ hw3/dist/main.js
@@ -1,5 +1,10 @@
 window.onload = function() {
-
+	var cancelBtn = document.getElementById('cancelBtn');
+	var textArea = document.getElementById('post');
+	cancelBtn.onclick = function () {
+		textArea.value = '';
+		textArea.innerText = '';
+	};
 };
 
 
