476cc99748de8030cf308fd06c608bf2bf8d1f7c
yusong-shen <yusongtju@gmail.com>
2016-09-20 09:38:54 -0500

    Complete Profile Page.

--- hw3/dist/profile.js
+++ hw3/dist/profile.js
@@ -1,15 +1,9 @@
-// The page will load with hardcoded values for each field. The submit button will determine 
-// which fields have changed and provide an alert to the user informing which fields are being updated. 
-// When the alert closes the updated values are displayed, the input fields should all be cleared. 
-// Be sure to validate the inputs (and perform password match) and alert the user of incorrect entries 
-// before updating values on the screen.
-// Style the profile page (you may include styles in the same file as for the main page).
+
 window.onload = function() {
 	var alertMsgs = {
 		'displayName' : '',
 		'phone' : '* Phone number format should look like : 123-123-1234\n',
 		'email' : '* Email format should look like : name@example.com\n',
-		'password' : '* Passwords you enter are not equal\n',
 		'zipcode' : '* Zipcode should be 5 digits like 77005\n'
 	};
 	var validatedREs = {
@@ -22,9 +16,7 @@ window.onload = function() {
 	function validateRe(re, value) {
 		return (value.match(re) !== null);
 	}
-	function validatePassword(pw, pw_cf) {
-		return pw === pw_cf;		
-	}
+
 	function clearContent() {
 		var allInputs = document.getElementsByTagName('input');
 		Array.prototype.forEach.call(allInputs, function(item) {
@@ -41,8 +33,10 @@ window.onload = function() {
 		// assume oldElement is span, newElement is input
 		var oldVal = oldElement.innerText;
 		var newVal = newElement.value;
-		window.alert(updatedMsg(field, oldVal, newVal));
-		oldElement.innerText = newVal;
+		var alert = oldElement.getElementsByClassName('alert')[0];
+		alert.className += ' alert-success';
+		alert.innerText = updatedMsg(field, oldVal, newVal);
+		oldElement.getElementsByClassName('currentVal')[0].innerText = newVal;
 	}
 	var submitBtn = document.getElementById('submit');
 	submitBtn.onclick = function () {
@@ -57,20 +51,14 @@ window.onload = function() {
 				isValidated = validateRe(validatedREs[item], newVal);
 			}
 			if (newVal && !isValidated) {
-				window.alert(alertMsgs[item]);				
+				var div = document.getElementsByClassName(item)[0];
+				var alert = div.getElementsByClassName('alert')[0];
+				alert.className += ' alert-danger';
+				alert.innerText = alertMsgs[item];
 			} else if (newVal && isValidated && oldVal !== newVal) {
 				updateField(item, oldElement, newElement);
 			}
 		});
-		// update password if user input them
-		var pwVal = document.getElementById('password').value;
-		var pwConfirmVal = document.getElementById('passwordConfirmation').value;
-		if (pwVal && pwConfirmVal && !validatePassword(pwVal, pwConfirmVal)) {
-			window.alert(alertMsgs['password']);
-		} else if (pwVal && pwConfirmVal && validatePassword(pwVal, pwConfirmVal)) {
-			window.alert('Password is updated!!');
-			document.getElementById('currentPwMsg').innerText = 'Password is updated!!';
-		}
 		// clear all the contents
 		clearContent();
 	}
