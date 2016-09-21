7198594c1a4372804ea6f5f6dc7249860d7efe2d
yusong-shen <yusongtju@gmail.com>
2016-09-20 20:23:28 -0500

    Implement update button

--- hw3/dist/main.js
+++ hw3/dist/main.js
@@ -5,4 +5,12 @@ window.onload = function() {
 		textArea.value = '';
 		textArea.innerText = '';
 	};
+
+	var updateBtn = document.getElementById('updateBtn');
+	var statusArea = document.getElementById('user-status');
+	updateBtn.onclick = function () {
+		var input = document.getElementById('input-status');
+		statusArea.innerText = input.value;
+		input.value = ''
+	}
 };
