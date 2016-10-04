ad0890c9f31d23d7d128d2022a200ebdea4356f5
yusong-shen <yusongtju@gmail.com>
2016-10-04 09:37:20 -0500

    Repair the basic animation.

--- hw4/README.md
+++ hw4/README.md
@@ -1,1 +1,12 @@
 #HW04 : Bear Fishing Game
+Author : Yusong Shen
+
+##Description
+There is a Ice Bear trying to catch some fish for dinner. The fish jump from the ice hole, and Ice Bear stand near the hole. When the fish jump out from the hole, Ice Bear use a baseball bat to hit the fish. If the player hit at the right moment, the fish will fly for some distances. The farther the fish flies, the higher score the player get.
+
+##Score : 
+How far a fish can fly is determined by the hitting angle. Player will have five opportunities, their score is the sum of these five hittings.
+If player hit fish in all their five attempts, they will get some bonus points.
+Some random gold/silver fish can appear with certain possibility, if player hit these fish, gold fish can bring you extra opportunities, silver fish can fly farther with extra bonus. 
+Normal fish will jump out from hole at regular speed within a certain range, silver and gold fish will jump at a higher speed hence harder to catch.
+The number of hit gold/silver fish can also be collected, which can be used as another ranking standard and the game can introduce badge system as well, like 'top gold fish catcher' etc.
--- hw4/index.js
+++ hw4/index.js
@@ -1,14 +1,34 @@
 'use strict';
 
-var modulo = function (x, y) {
+function modulo(x, y) {
     return ((x % y) + y) % y;
 };
+
+function init() {
+    var canvas = document.createElement('canvas');
+    canvas.width = 600;
+    canvas.height = 400;
+    document.body.childNodes[1].appendChild(canvas);
+
+    return canvas;
+}
+
+function main() {
+    var now = Date.now();
+
+    // update();
+    // render();
+
+    window.requestAnimationFrame(main);
+}
 
 var createApp = function (canvas) {
     // canvas : 600 * 400
     var c = canvas.getContext("2d");
     var floor = canvas.height - canvas.height / 5;
-
+    var dx = 2;
+    var sunX = 100;
+    var sunY = 50;
 
     var draw_ground = function (floor) {
         // Create the ground
@@ -30,7 +50,34 @@ var createApp = function (canvas) {
         c.fill();
     };
 
-    
+    // fish image size : 182 * 198
+    var fishImg = new Image();
+    // bear image size : 274 * 370
+    var bearImg = new Image();
+    var holeX = 0.8 * canvas.width;
+    var bearX = 0.85 * canvas.width;
+    var bearWidth = 0.1 * canvas.width;
+    var bearHeight = 1.5 * bearWidth;
+    var bearY = floor - bearHeight;
+    // c.drawImage(bearImg, bearX, bearY, bearWidth, bearHeight);
+    // have to wait for image loading
+    bearImg.onload = function () {
+        c.drawImage(bearImg, bearX, bearY, bearWidth, bearHeight);
+    };
+    bearImg.src = "/img/ice-bear.png";
+
+
+    var fishSize = 0.5 * bearWidth;
+    var fishX = holeX - 0.5 * fishSize;
+    var fishY = floor - fishSize;
+    // c.drawImage(fishImg, fishX, fishY, fishSize, fishSize);
+
+    fishImg.onload = function () {
+        c.drawImage(fishImg, fishX, fishY, fishSize, fishSize);
+    };
+    fishImg.src = "/img/gray-fish.png";
+
+
     var draw = function () {
         // clear the canvas above floor
         c.clearRect(0, 0, canvas.width, floor);
@@ -39,33 +86,50 @@ var createApp = function (canvas) {
         var holeX = 0.8 * canvas.width;
         draw_hole(30, "#BBDEFB", holeX, floor);
 
-        // bear image size : 274 * 370
-        var bearImg = new Image();
-        bearImg.src = "/img/ice-bear.png";
-        var bearX = 0.85 * canvas.width;
-        var bearWidth = 0.1 * canvas.width;
-        var bearHeight = 1.5 * bearWidth;
-        var bearY = floor - bearHeight;
+        // draw the sun
+        var radius = 30;
+        c.beginPath();
+        c.arc(sunX, sunY, radius, 0, 2 * Math.PI, false);
+        sunX = modulo(sunX + dx, canvas.width);
+        c.fillStyle = "orange";
+        c.fill();
+
+
+
+        bearX = 0.85 * canvas.width;
+        bearWidth = 0.1 * canvas.width;
+        bearHeight = 1.5 * bearWidth;
+        bearY = floor - bearHeight;
+        c.drawImage(bearImg, bearX, bearY, bearWidth, bearHeight);
         // have to wait for image loading
-        bearImg.onload = function () {
-            c.drawImage(bearImg, bearX, bearY, bearWidth, bearHeight);
-        };
+        // bearImg.onload = function () {
+        //     c.drawImage(bearImg, bearX, bearY, bearWidth, bearHeight);
+        // };
+        // bearImg.src = "/img/ice-bear.png";
 
-        // fish image size : 182 * 198
-        var fishImg = new Image();
-        fishImg.src = "/img/gray-fish.png";
-        var fishSize = 0.5 * bearWidth;
-        var fishX = holeX - 0.5 * fishSize;
-        var fishY = floor - fishSize;
-        fishImg.onload = function () {
-            c.drawImage(fishImg, fishX, fishY, fishSize, fishSize);
-        };
-    }
 
-    setInterval(draw, 1000);
+        fishSize = 0.5 * bearWidth;
+        fishX = modulo(fishX - 2* dx, canvas.width);
+        fishY = floor - fishSize;
+        c.drawImage(fishImg, fishX, fishY, fishSize, fishSize);
+
+        // fishImg.onload = function () {
+        //     c.drawImage(fishImg, fishX, fishY, fishSize, fishSize);
+        // };
+        // fishImg.src = "/img/gray-fish.png";
+
+        // window.requestAnimationFrame(draw);
+    };
+
+    return {
+        draw: draw
+    }    
 };
 
 window.onload = function () {
-    var app = createApp(document.querySelector("canvas"));
-
+    var canvas = init();
+    var app = createApp(canvas);
+    // app.draw();
+    var frame_rate = 1000 / 30;
+    setInterval(app.draw, frame_rate);
 };
