aa2a7344fd5391319651ec39a58f80db6a95b7bb
yusong-shen <yusongtju@gmail.com>
2016-10-06 21:28:00 -0500

    solve inclass-14

--- inclass-14/e2e/login.spec.js
+++ inclass-14/e2e/login.spec.js
@@ -22,17 +22,11 @@ describe('Test Dummy Server Example Page', () => {
     it("Update the headline and verify the change", (done) => {
         // IMPLEMENT ME
         var username = common.creds.username;
-        var prefix = `you are logged in as ${username} `;
-        var oldHeadline = '';
-        var newHeadline = 'I am new headline three!';
+        var prefix = `you are logged in as ${username} `
+        var oldHeadline = 'I am old headline!'
+        var newHeadline = 'I am new headline three!'
         sleep(500)
-        // recored the old headline
-        .then(findId('message').getText()
-            .then(text => {
-                oldHeadline = text
-            }))
         // find the headline input
-        // .sendKeys(new headline message)
         .then(findId('newHeadline').sendKeys(newHeadline))
         .then(findId('headline').click())
         .then(sleep(2000))
@@ -40,23 +34,22 @@ describe('Test Dummy Server Example Page', () => {
         .then(findId('message').getText()
             .then(text => {
                 expect(text).to.equal(prefix + '"' + newHeadline + '"')
-            })
-            .then(done))
+            }))
         // .sendKeys(the old headline message)
         // verify the headline is updated
-        // `you are logged in as ${response.username} "${response.headline}"`
-        // .then(sleep(1000))
-        // .then(
-        //     findId('newHeadline').sendKeys(oldHeadline.substring(prefix.length)))
-        // .then(findId('headline').click())
-        // .then(sleep(2000))
-        // // verify the headline is updated
-        // .then(findId('message').getText()
-        //     .then(text => {
-        //         expect(text).to.equal(oldHeadline)
-        //     })
-        //     .then(done))
+        .then(sleep(1000))
+        .then(findId('newHeadline').clear())
+        .then(findId('newHeadline').sendKeys(oldHeadline))        
+        .then(findId('headline').click())
+        .then(sleep(2000))
+        // verify the headline is updated
+        .then(findId('message').getText()
+            .then(text => {
+                expect(text).to.equal(prefix + '"' + oldHeadline + '"')
+            })
+            .then(done))
     })
+
 
     after('should log out', (done) => {
         common.logout().then(done)
