ecb3979840154d8eae0047c5474c3da858aec8bd
yusong-shen <yusongtju@gmail.com>
2016-10-24 14:48:21 -0500

    Use Redux Provider wrapper.

--- hw5/src/components/auth/authActions.js
+++ hw5/src/components/auth/authActions.js
@@ -1,3 +1,51 @@
 /**
  * Created by yusong on 10/20/16.
  */
+
+const url = 'https://webdev-dummy.herokuapp.com'
+
+const resource = (method, endpoint, payload) => {
+    const options =  {
+        method,
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    if (payload) options.body = JSON.stringify(payload)
+
+    return fetch(`${url}/${endpoint}`, options)
+        .then(r => {
+            if (r.status === 200) {
+                return (r.headers.get('Content-Type').indexOf('json') > 0) ? r.json() : r.text()
+            } else {
+                // useful for debugging, but remove in production
+                console.error(`${method} ${endpoint} ${r.statusText}`)
+                throw new Error(r.statusText)
+            }
+        })
+}
+
+const login = () => {
+
+    const username = document.querySelector("#username").value
+    const password = document.querySelector("#password").value
+
+    const box = document.querySelector("#message")
+    return resource('POST', 'login', { username, password })
+        .then(r => resource('GET', 'headlines'))
+        .then(r => {
+            const user = r.headlines[0]
+            box.innerHTML = `you are logged in as ${user.username} "${user.headline}"`
+        })
+        .catch(r => box.innerHTML = `"${r.message || 'Error'}" when logging in`)
+}
+
+const logout = () => {
+    const box = document.querySelector("#message")
+    return resource('PUT', 'logout')
+        .then(r => box.innerHTML = "You have logged out" )
+        .catch(r => box.innerHTML = `"${r.message}" when logging out` )
+}
+
+//export { url, login, logout }
--- hw5/src/index.js
+++ hw5/src/index.js
@@ -7,10 +7,19 @@ require('./styles.css')
 
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import createLogger from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux'
 
 import App from './components/app'
+import Reducer from './reducers'
+
+const logger = createLogger()
+const store = createStore(Reducer, applyMiddleware(logger))
 
 render(
-    <App />,
-    document.getElementById('app')
+	<Provider store={store}>
+        <App />
+	</Provider>,
+	document.getElementById('app')
 )
