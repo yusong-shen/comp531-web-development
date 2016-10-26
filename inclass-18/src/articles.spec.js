70b94048b14b78059538fb194d8a0311fe10b8af
yusong-shen <yusongtju@gmail.com>
2016-10-25 22:15:16 -0500

    Add ES6 Promise Polifill, still can't post

--- inclass-18/src/articles.spec.js
+++ inclass-18/src/articles.spec.js
@@ -2,6 +2,7 @@
  * Test suite for articles.js
  */
 const expect = require('chai').expect
+require('es6-promise').polyfill()
 const fetch = require('isomorphic-fetch')
 
 const url = path => `http://localhost:3000${path}`
@@ -23,6 +24,48 @@ describe('Validate Article functionality', () => {
 		.then(done)
 		.catch(done)
 	}, 500)
+
+	it('should return an article with a specified id', (done) => {
+		fetch(url('/articles'))
+		.then(res => {
+			expect(res.status).to.eql(200)
+			return res.json()
+		})
+		// call GET /articles first to find an id, perhaps one at random
+		.then(body => {
+			const idx = Math.floor(Math.random() * body.articles.length)
+			return body.articles[idx]
+		})
+		// then call GET /articles/id with the chosen id
+		// validate that only one article is returned	
+		.then(article => {
+			fetch(url(`/articles/${article.id}`))
+			.then(res => {
+				expect(res.status).to.eql(200)
+				return res.json()
+			})
+			.then(body => {
+				expect(body.articles.length).to.eql(1)
+			})
+		})
+		.then(done)
+		.catch(done)
+	}, 200)
+
+	it('should return nothing for an invalid id', (done) => {
+		// call GET /articles/id where id is not a valid article id, perhaps 0
+		// confirm that you get no results
+		fetch(url('/articles/0'))
+		.then(res => {
+			expect(res.status).to.eql(200)
+			return res.json()
+		})
+		.then(body => {
+			expect(body.articles.length).to.eql(0)
+		})
+		.then(done)
+		.catch(done)
+	}, 200)
 
 	it('should add two articles with successive article ids, and return the article each time', (done) => {
 		// add a new article
@@ -69,47 +112,5 @@ describe('Validate Article functionality', () => {
 		.then(done)
 		.catch(done)
  	}, 500)
-
-	it('should return an article with a specified id', (done) => {
-		fetch(url('/articles'))
-		.then(res => {
-			expect(res.status).to.eql(200)
-			return res.json()
-		})
-		// call GET /articles first to find an id, perhaps one at random
-		.then(body => {
-			const idx = Math.floor(Math.random() * body.articles.length)
-			return body.articles[idx]
-		})
-		// then call GET /articles/id with the chosen id
-		// validate that only one article is returned	
-		.then(article => {
-			fetch(url(`/articles/${article.id}`))
-			.then(res => {
-				expect(res.status).to.eql(200)
-				return res.json()
-			})
-			.then(body => {
-				expect(body.articles.length).to.eql(1)
-			})
-		})
-		.then(done)
-		.catch(done)
-	}, 200)
-
-	it('should return nothing for an invalid id', (done) => {
-		// call GET /articles/id where id is not a valid article id, perhaps 0
-		// confirm that you get no results
-		fetch(url('/articles/0'))
-		.then(res => {
-			expect(res.status).to.eql(200)
-			return res.json()
-		})
-		.then(body => {
-			expect(body.articles.length).to.eql(0)
-		})
-		.then(done)
-		.catch(done)
-	}, 200)
 
 });
