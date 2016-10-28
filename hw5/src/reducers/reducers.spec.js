bc5c48a6bc90586cad0a1144956cb2bad9a6c486
yusong-shen <yusongtju@gmail.com>
2016-10-27 22:33:43 -0500

    Add coverage report

---
+++ hw5/src/reducers/reducers.spec.js
@@ -0,0 +1,35 @@
+import { expect } from 'chai'
+import rootReducer from './reducers'
+
+describe('Validate reducer', () => {
+
+    it('should initialize state', (done) => {
+        // TODO
+        done()
+    })
+
+    it('should state success (for displaying success message to user)', (done) => {
+        // TODO
+        done()
+    })
+
+    it('should state error (for displaying error message to user)', (done) => {
+        // TODO
+        done()
+    })
+
+    it('should set the articles', (done) => {
+        // TODO
+        done()
+    })
+
+    it('should set the search keyword', (done) => {
+        // TODO
+        done()
+    })
+
+    it('should filter displayed articles by the search keyword', (done) => {
+        // TODO
+        done()
+    })
+})
